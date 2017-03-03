953a07efd6014a50d853e503b33560e39a59b7d6
Jody Salt <jody.salt@stfc.ac.uk>
2017-03-03 15:58:56 +0000

    partially implemented screen sharing autocomplete

--- src/main/webapp/scripts/controllers/share-machine.js
+++ src/main/webapp/scripts/controllers/share-machine.js
@@ -10,25 +10,38 @@
 
         this.newUser = "";
         this.users = [];
+        this.candidateUsers = [];
+
 
         this.deleteUser = function(user){
             _.remove(this.users, user);
         };
 
-        this.addUser = function(){
-            if(this.newUser == '') return;
+        this.updateCandidateUsers = function(){
+            if(this.newUser == ""){
+                this.candidateUsers = [];
+            } else {
+                facility.icat().query([
+                    "select user from User user",
+                    "where user.name like concat('%', ?, '%')", this.newUser,
+                    "or user.fullName like concat('%', ?, '%')", this.newUser,
+                    "limit 0, 11"
+                ]).then(function(users){
+                    that.candidateUsers = users
+                });
+            }
+        };
 
-            facility.icat().query([
-                "select user from User user",
-                "where user.name like concat('%', ?, '%')", this.newUser,
-                "or user.fullName like concat('%', ?, '%')", this.newUser,
-                "limit 0, 2"
-            ]).then(function(users){
-                if(users.length == 1){
-                    that.users.push(users[0]);
-                    that.newUser = '';
-                }
-            });
+        this.addUser = function(user){
+            if(user){
+                this.users.push(user);
+                this.newUser = '';
+                this.candidateUsers = [];
+            } else if(this.candidateUsers.length == 1){
+                this.users.push(this.candidateUsers[0]);
+                this.newUser = '';
+                this.candidateUsers = [];
+            }
         };
 
         this.share = function() {
