8c5e9ab38841e3f5b7894a7f0df2b4c521d26a87
Jody Salt <jody.salt@stfc.ac.uk>
2017-03-03 16:40:39 +0000

    fully implemented screen sharing autocomplete

--- src/main/webapp/scripts/controllers/share-machine.js
+++ src/main/webapp/scripts/controllers/share-machine.js
@@ -11,10 +11,44 @@
         this.newUser = "";
         this.users = [];
         this.candidateUsers = [];
+        this.selectedCandidateUserPosition = -1;
 
 
         this.deleteUser = function(user){
             _.remove(this.users, user);
+        };
+
+        this.newUserKeydown =  function($event){
+            if($event.which == 13){
+                this.addUser();
+                $event.preventDefault();
+            }
+
+            if($event.which == 38){
+                this.newUserUpKey();
+                $event.preventDefault();
+            }
+
+            if($event.which == 40){
+                this.newUserDownKey();
+                $event.preventDefault();
+            }
+        };
+
+        this.newUserKeyup = function($event){
+            if(!($event.which == 38 || $event.which == 40)){
+                this.updateCandidateUsers();
+            }
+        };
+
+        this.newUserUpKey = function(){
+            this.selectedCandidateUserPosition--;
+            if(this.selectedCandidateUserPosition < -1) this.selectedCandidateUserPosition = -1;
+        };
+
+        this.newUserDownKey = function(){
+            this.selectedCandidateUserPosition++;
+            if(this.selectedCandidateUserPosition > this.candidateUsers.length - 1) this.selectedCandidateUserPosition = this.candidateUsers.length - 1;
         };
 
         this.updateCandidateUsers = function(){
@@ -27,7 +61,11 @@
                     "or user.fullName like concat('%', ?, '%')", this.newUser,
                     "limit 0, 11"
                 ]).then(function(users){
-                    that.candidateUsers = users
+                    that.candidateUsers = _.map(users, function(user, position){
+                        user.position = position;
+                        return user;
+                    });
+                    that.selectedCandidateUserPosition = -1;
                 });
             }
         };
@@ -35,6 +73,10 @@
         this.addUser = function(user){
             if(user){
                 this.users.push(user);
+                this.newUser = '';
+                this.candidateUsers = [];
+            } else if(this.selectedCandidateUserPosition > -1){
+                this.users.push(this.candidateUsers[this.selectedCandidateUserPosition]);
                 this.newUser = '';
                 this.candidateUsers = [];
             } else if(this.candidateUsers.length == 1){
